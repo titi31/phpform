75a044e3a57be5d0a788e9513e1367e2fe518f3f
Mr.Robot <classic.graal095@gmail.com>
2020-03-06 19:56:13 +0100

    indentation

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
 <html>
-<head>
-	<title>salut</title>
-	<meta charset="utf-8">
-</head>
-<body>
+	<head>
+		<title>salut</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
 	
-<?php
-	function afficherFormulaireInscrption(){
-	echo "<form method='post'>
-	<input type='text'placeholder='pseudo'><br>
-	<input type='mail'placeholder='mail'><br>
-	<input type='text'placeholder='bonjour merci aurevoir'><br>
-	<input type='submit'value='enregistre'></form>";
-}
-afficherFormulaireInscrption();
-?>
-</body>
+		<?php
+			function afficherFormulaireInscrption()
+			{
+		
+			echo 
+			"
+			<form method='post'>
+			<input type='text'placeholder='pseudo'><br>
+			<input type='mail'placeholder='mail'><br>
+			<input type='text'placeholder='bonjour merci aurevoir'><br>
+			<input type='submit'value='enregistre'></form>
+			";
+			}
+	
+			afficherFormulaireInscrption();
+		?>
+	</body>
 </html>
