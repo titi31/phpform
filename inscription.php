e5dbb7f5b53bd57f2b494672da8c4f2bbe26855c
Mr.Robot <classic.graal095@gmail.com>
2020-03-06 20:09:03 +0100

    indentation

--- inscription.php
+++ inscription.php
@@ -10,157 +10,143 @@
   <a href="http://www.perdu.com">error</a>
 </div> 
 <br>
-
 		<?php
-		afficherEntete();
-		
 
-		//aiguillage
-		
-		
-		if (isset($_REQUEST['choix'])){
-			switch ($_REQUEST ['choix']) {
+		afficherEntete();
+		if (isset($_REQUEST['choix']))
+		{
+			switch ($_REQUEST ['choix']) 
+			{
 				case 'inscription':
 
 				     afficherFormulaireInscrption();
-					// code...
+				     break;
+				case 'enregistre':
+				     include('Membre.php');
+				     $membre= new Membre(null,$_REQUEST['pseudo'],$_REQUEST['mail'],$_REQUEST['chaineMots']);
+				     $membre->save();
+				     echo 'je vais traiter le formulaire';
+				     break;
+				case 'connecter':
+				     include('Membre.php');
+				     $membre=new Membre($_REQUEST['id']);
+				     
+				     if($membre->getPseudo()==null)
+				     {
+					afficherAccueil();
+				        echo "user n'existe pas reessayer";
+				     }
+				     else
+				     {
+					echo "<div id='monCompte'>".$membre->getPseudo()."<br> ".
+					      $membre->getMail()."<br> ".$membre->getChaineMots()."
+					      <form method='post'><input type='hidden' value='".$_REQUEST['id']
+					      ."' name='id'><button type='submit' value='deluser' name='choix'>
+					      delete user</button><br><button type='submit' value='upuser' name='choix'>
+					      mettre a jour user</button></div>
+					      <button type='submit' value='mots' name='choix'>chercher mot commun</button>
+					      </form>";
+				     }
+				     break;	
+				case 'mots':
+					include("Membre.php");
+					$membre=new Membre($_REQUEST['id']);
+				        $mots=$membre->motCommun();
+								
+					foreach($mots as $cle => $valeur)
+					{
+					 	echo $cle." a comme mot commun : ".$valeur."<br>";
+					}
+					
+					if($mots==null)
+						echo "pas de mots commun";
+						break;
+					
+				case 'deluser':
+					include('Membre.php');
+					$membre=new Membre($_REQUEST['id']);
+					$membre->delete();
+					echo  $membre->getPseudo()." a été supprimer";
 					break;
-					case 'enregistre':
-						include('Membre.php');
-						$membre= new Membre(null,$_REQUEST['pseudo'],$_REQUEST['mail'],$_REQUEST['chaineMots']);
-						$membre->save();
-						//enregistrerForm();
-						echo 'je vais traiter le formulaire';
-						break;
-					case 'connecter':
-							include('Membre.php');
-							$membre=new Membre($_REQUEST['id']);
-							if($membre->getPseudo()==null){
-								afficherAccueil();
-								echo "user n'existe pas reessayer";
-							}
-							else{
-							echo "<div id='monCompte'>".$membre->getPseudo()."<br> ".$membre->getMail()."<br> ".$membre->getChaineMots()."
-								<form method='post'><input type='hidden' value='".$_REQUEST['id']."' name='id'>
-								<button type='submit' value='deluser' name='choix'>delete user</button><br>
-								
-								<button type='submit' value='upuser' name='choix'>mettre a jour user</button></div>
-								<button type='submit' value='mots' name='choix'>chercher mot commun</button>
-								</form>";}
-							break;	
-					case 'mots':
-								include("Membre.php");
-								
-								$membre=new Membre($_REQUEST['id']);
-								$mots=$membre->motCommun();
-								
-								foreach($mots as $cle => $valeur){
-									echo $cle." a comme mot commun : ".$valeur."<br>";
-								}
-								if($mots==null)
-									echo "pas de mots commun";
-								break;
-					case 'deluser':
-						include('Membre.php');
-						$membre=new Membre($_REQUEST['id']);
-						$membre->delete();
-						echo  $membre->getPseudo()." a été supprimer";
-						break;
-					case 'upuser':
-						include('Membre.php');
-						$membre=new Membre($_REQUEST['id']);
-							modifier($membre);
-							break;
-					//if(isset($_REQUEST['choix1'])){
-					case "updateb":	
-						include('Membre.php');
-
-						$membre=new Membre($_REQUEST['up']);
-						$membre->setPseudo($_REQUEST['pseudoupdate']);
-						$membre->setMail($_REQUEST['mailupdate']);
-						$membre->setChaineMots($_REQUEST['listeupdate']);
-						$membre->update();
-						
-						echo "bien modifié";
-						break;
-					case "pseudo":
-						include("Membre.php");
-						afficherAccueil();
-						echo  "<div id='liste'>";
-						$liste=Membre::getListe();
-						foreach($liste as $membre ){
-							echo $membre->getPseudo()." ".$membre->getMail()."<br>";
-						}
-
-						/*for($i=0;$i<sizeof($liste);$i++){
-							echo $liste[$i]."<br>";
-						}*/
-						echo "</div>";
-						break;
-					case 'cacher':
-						afficherAccueil();
-						echo "<style type='text/css'>#liste{display:none;}</style>";
-						break;
-
+				case 'upuser':
+					include('Membre.php');
+					$membre=new Membre($_REQUEST['id']);
+					modifier($membre);
+					break;
+				case "updateb":	
+					include('Membre.php');
+					$membre=new Membre($_REQUEST['up']);
+					$membre->setPseudo($_REQUEST['pseudoupdate']);
+					$membre->setMail($_REQUEST['mailupdate']);
+					$membre->setChaineMots($_REQUEST['listeupdate']);
+					$membre->update();	
+					echo "bien modifié";
+					break;
+				case "pseudo":
+					include("Membre.php");
+					afficherAccueil();
+					echo  "<div id='liste'>";
+					$liste=Membre::getListe();
+					
+					foreach($liste as $membre )
+					{
+						echo $membre->getPseudo()." ".$membre->getMail()."<br>";
+					}
+					echo "</div>";
+					break;
+				case 'cacher':
+					afficherAccueil();
+					echo "<style type='text/css'>#liste{display:none;}</style>";
+					break;
 				default:
-					// code...
 					break;
 			}
 	
 		}
-		else{
+		else
+		{
 			afficherAccueil();
 		}
+
 		afficherBas();
 
-	/*function enregistrerForm(){
-		include("connectionDB.php");
-		$sql = "INSERT INTO Membres(pseudo ,mail, chaineMots) VALUES(?, ?, ?);";
-						$stmt = $bdd->prepare($sql);   // préparation de la requète
-						$stmt->bindParam(1, $_REQUEST['pseudo']);
-						$stmt->bindParam(2, $_REQUEST['mail']);
-						$stmt->bindParam(3, $_REQUEST['chaineMots']);
-						if ($stmt->execute()==true)
-							$id = $bdd->lastInsertId(); // recup id auto-incrementé généré par la base
-						else 
-							die ('erreur requete '.$sql);
-	
-						
+	function afficherFormulaireInscrption()
+	{
+		echo "<form method='post'>
+		<input type='text'placeholder='pseudo' name='pseudo'><br>
+		<input type='mail'placeholder='mail' name='mail'><br>
+		<input type='text'placeholder='bonjour merci aurevoir' name='chaineMots'><br>
+		<input type='submit'value='enregistre' name='choix'></form>";
 	}
-*/
-	function afficherFormulaireInscrption(){
-	echo "<form method='post'>
-	<input type='text'placeholder='pseudo' name='pseudo'><br>
-	<input type='mail'placeholder='mail' name='mail'><br>
-	<input type='text'placeholder='bonjour merci aurevoir' name='chaineMots'><br>
-	<input type='submit'value='enregistre' name='choix'></form>";
-}
-function modifier($membre){
+
+	function modifier($membre)
+	{
+		echo "<form method='post'>
+		     <input type='text' value='".$membre->getPseudo()."' name='pseudoupdate'>
+		     <input type='text' value='".$membre->getMail()."' name='mailupdate'>
+		     <input type='text' value='".$membre->getChaineMots()."' name='listeupdate'>
+		     <input type='hidden' value='".$_REQUEST['id']."' name='up'>
+		     <button type='submit' value='updateb' name='choix'>mettre a jour</button>
+		     </form>";
+	}
 	
-						echo "<form method='post'>
-							<input type='text' value='".$membre->getPseudo()."' name='pseudoupdate'>
-							<input type='text' value='".$membre->getMail()."' name='mailupdate'>
-							<input type='text' value='".$membre->getChaineMots()."' name='listeupdate'>
-							<input type='hidden' value='".$_REQUEST['id']."' name='up'>
-							<button type='submit' value='updateb' name='choix'>mettre a jour</button>
-							</form>"	;
-}
-	
-	function afficherEntete(){
+	function afficherEntete()
+	{
 		echo "<!DOCTYPE html>
-				<html>
-					<head>
-						<title>inscription</title>
-						<meta charset='utf-8'>
-						<link rel='stylesheet' href='style.css'>
-
-					</head>
-					<body>";
+		      <html>
+			<head>
+				<title>inscription</title>
+				<meta charset='utf-8'>
+				<link rel='stylesheet' href='style.css'>
+			</head>
+			<body>";
 	}
 
-	function afficherAccueil(){
+	function afficherAccueil()
+	{
 		echo "<form method='post' action='' id='home'>
-		<div id='boutonInscrire'><button type='submit'value='inscription' name='choix'>inscription</button></div><br>
+		<div id='boutonInscrire'><button type='submit'value='inscription' name='choix'>inscription</button>
+		</div><br>
 		<input type='text' placeholder='tape le numero de id' name='id'>
 		<button type='submit' value='connecter' name='choix'>se Connecter</button><br>
 		<button type='submit' value='pseudo' name='choix'>afficher user</button>
@@ -169,7 +155,8 @@ function modifier($membre){
 		
 
 	}
-	function afficherBas(){
+	function afficherBas()
+	{
 		echo "</body>
 			</html>";
 	}
